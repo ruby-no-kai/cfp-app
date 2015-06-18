533368892e50c58cbce1c803863e06bff2f2d33a
Akira Matsuda <ronnie@dio.jp>
2015-06-18 18:40:56 +0900

    Show GitHub, Twitter accounts on speaker information

--- app/decorators/speaker_decorator.rb
+++ app/decorators/speaker_decorator.rb
@@ -6,4 +6,32 @@ class SpeakerDecorator < Draper::Decorator
   def bio
     object.bio.present? ? object.bio : object.user.try(:bio)
   end
+
+  def github_account
+    user.identities.where(provider: 'github').pick(:account_name)
+  end
+
+  def twitter_account
+    user.identities.where(provider: 'twitter').pick(:account_name)
+  end
+
+  def social_account
+    twitter_account || github_account || (user&.name || speaker_name).downcase.tr(' ', '_')
+  end
+
+  def link_to_github
+    if (uname = github_account)
+      h.link_to "@#{uname}", "https://github.com/#{uname}", target: '_blank'
+    else
+      'none'
+    end
+  end
+
+  def link_to_twitter
+    if (uname = twitter_account)
+      h.link_to "@#{uname}", "https://twitter.com/#{uname}", target: '_blank'
+    else
+      'none'
+    end
+  end
 end
