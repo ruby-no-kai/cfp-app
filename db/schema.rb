d6a6536f4a431b605c24c41a84183ca3a29a706d
Akira Matsuda <ronnie@dio.jp>
2020-02-14 21:02:47 +0900

    rails g migration add_github_account_to_users github_account

--- db/schema.rb
+++ db/schema.rb
@@ -263,6 +263,7 @@ ActiveRecord::Schema.define(version: 2021_05_24_020710) do
     t.datetime "updated_at"
     t.string "github_uid"
     t.string "twitter_uid"
+    t.string "github_account"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token"
     t.index ["email"], name: "index_users_on_email"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
