1398409857bcf20cff599a4c583dd8b71309b807
Akira Matsuda <ronnie@dio.jp>
2021-08-09 18:30:12 +0900

    github_account / twitter_account can be ''

--- app/decorators/speaker_decorator.rb
+++ app/decorators/speaker_decorator.rb
@@ -8,11 +8,11 @@ class SpeakerDecorator < Draper::Decorator
   end
 
   def github_account
-    user.identities.where(provider: 'github').pick(:account_name) if user
+    user.identities.where(provider: 'github').pick(:account_name).presence if user
   end
 
   def twitter_account
-    user.identities.where(provider: 'twitter').pick(:account_name) if user
+    user.identities.where(provider: 'twitter').pick(:account_name).presence if user
   end
 
   def social_account
