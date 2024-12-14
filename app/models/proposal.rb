f3945c5c987e4eddf19ff65b45c8953ecec8876c
Akira Matsuda <ronnie@dio.jp>
2024-12-14 11:35:34 +0900

    Let new proposal's default spoken_language be 'EN'

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -164,7 +164,7 @@ class Proposal < ApplicationRecord
   end
 
   def spoken_language
-    super || RubyKaigi::CfpApp.spoken_language(custom_fields['spoken language in your talk'])
+    super || RubyKaigi::CfpApp.spoken_language(custom_fields['spoken language in your talk'] || 'EN')
   end
 
   def confirm
